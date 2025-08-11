8aa2ed64007aef16ff78e23fbc9b24db99f2b704
Ahmed Hanie <flubzd@gmail.com>
2025-08-11 22:41:14 +0300

    temp

---
+++ source/OnHitWorkers/AddResearch.cs
@@ -0,0 +1,33 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Infusion.OnHitWorkers
+{
+    public class AddResearch: OnHitWorker
+    {
+        public AddResearch()
+        {
+        }
+
+        public override void BulletHit(ProjectileRecord record)
+        {
+            AddResearchPoints(record.target, record.projectile.Launcher as Pawn);
+        }
+
+        public override void MeleeHit(VerbRecordData record)
+        {
+            AddResearchPoints(record.target, record.verb.CasterPawn);
+        }
+
+        private void AddResearchPoints(Thing target, Pawn caster)
+        {
+            ResearchProjectDef project = Find.ResearchManager.GetProject();
+            if (project == null)
+            {
+                return;
+            }
+            Find.ResearchManager.ResearchPerformed(amount, caster);
+            MoteMaker.ThrowText(caster.Position.ToVector3(), caster.MapHeld, "Infusion.Savant.Message".Translate());
+        }
+    }
+}
