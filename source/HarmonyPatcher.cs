29851aa10a0b9b170e3bd81ee718e2003010e900
Ahmed Hanie <flubzd@gmail.com>
2025-08-12 10:43:04 +0300

    fix: compatibility with Toggleable Shields
    - Disable patching methods related to shielding for the Aegis infusion when Toggleable Shields mod is detected

--- source/HarmonyPatcher.cs
+++ source/HarmonyPatcher.cs
@@ -11,18 +11,23 @@ namespace Infusion
             MethodInfo original = AccessTools.Method(typeof(Pawn_ApparelTracker), "Notify_ApparelAdded");
             HarmonyMethod postfix = new HarmonyMethod(typeof(Patches).GetMethod("NotifyApparelAddedPostfix"));
             instance.Patch(original, null, postfix);
-            MethodInfo original2 = AccessTools.PropertyGetter(typeof(CompShield), "EnergyGainPerTick");
-            HarmonyMethod postfix2 = new HarmonyMethod(typeof(Patches).GetMethod("EnergyGainPerTickPostfix"));
-            instance.Patch(original2, null, postfix2);
-            MethodInfo original3 = AccessTools.PropertyGetter(typeof(CompShield), "EnergyMax");
-            HarmonyMethod postfix3 = new HarmonyMethod(typeof(Patches).GetMethod("EnergyMaxGetterPostfix"));
-            instance.Patch(original3, null, postfix3);
+
+            if (!Compat.ToggleableShields.Enabled)
+            {
+                MethodInfo original2 = AccessTools.PropertyGetter(typeof(CompShield), "EnergyGainPerTick");
+                HarmonyMethod postfix2 = new HarmonyMethod(typeof(Patches).GetMethod("EnergyGainPerTickPostfix"));
+                instance.Patch(original2, null, postfix2);
+                MethodInfo original3 = AccessTools.PropertyGetter(typeof(CompShield), "EnergyMax");
+                HarmonyMethod postfix3 = new HarmonyMethod(typeof(Patches).GetMethod("EnergyMaxGetterPostfix"));
+                instance.Patch(original3, null, postfix3);
+                MethodInfo original5 = AccessTools.Method(typeof(Gizmo_EnergyShieldStatus), "GizmoOnGUI");
+                HarmonyMethod transpiler = new HarmonyMethod(typeof(Patches).GetMethod("GizmoOnGUITranspiler"));
+                instance.Patch(original5, null, null, transpiler);
+            }
             MethodInfo original4 = AccessTools.Method(typeof(Pawn_ApparelTracker), "Notify_ApparelRemoved");
             HarmonyMethod postfix4 = new HarmonyMethod(typeof(Patches).GetMethod("NotifyApparelRemovedPostfix"));
             instance.Patch(original4, null, postfix4);
-            MethodInfo original5 = AccessTools.Method(typeof(Gizmo_EnergyShieldStatus), "GizmoOnGUI");
-            HarmonyMethod transpiler = new HarmonyMethod(typeof(Patches).GetMethod("GizmoOnGUITranspiler"));
-            instance.Patch(original5, null, null, transpiler);
+           
         }
     }
 }
