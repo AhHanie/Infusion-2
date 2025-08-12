f3a8f93ec909c693b10a5b1af74c2e97841854a4
Ahmed Hanie <flubzd@gmail.com>
2025-08-13 01:07:20 +0300

    temp gas testing

---
+++ source/OnHitWorkers/SpawnGas.cs
@@ -0,0 +1,25 @@
+﻿
+using Verse;
+
+namespace Infusion.OnHitWorkers
+{
+    public class SpawnGas : OnHitWorker
+    {
+        GasType gas;
+        public SpawnGas() 
+        {
+            gas = GasType.BlindSmoke;
+        }
+
+        public override void BulletHit(ProjectileRecord record)
+        {
+            Log.Message($"THE TRUTH: {record.projectile.Position.ToString()}, {gas}");
+            if (record.projectile.MapHeld == null)
+            {
+                Log.Message("Map Held is null ..");
+            }
+            
+            GasUtility.AddGas(record.projectile.Position, record.projectile.MapHeld, gas, amount);
+        }
+    }
+}
