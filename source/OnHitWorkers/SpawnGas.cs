2c4eaafc5e8ebdbaa190b4c4a44125cc8b53cae4
Ahmed Hanie <flubzd@gmail.com>
2025-08-13 01:53:49 +0300

    feat: add toxigen and foulblight infusions

--- source/OnHitWorkers/SpawnGas.cs
+++ source/OnHitWorkers/SpawnGas.cs
@@ -13,13 +13,7 @@ namespace Infusion.OnHitWorkers
 
         public override void BulletHit(ProjectileRecord record)
         {
-            Log.Message($"THE TRUTH: {record.projectile.Position.ToString()}, {gas}");
-            if (record.projectile.MapHeld == null)
-            {
-                Log.Message("Map Held is null ..");
-            }
-            
-            GasUtility.AddGas(record.projectile.Position, record.projectile.MapHeld, gas, amount);
+            GasUtility.AddGas(record.projectile.Position, record.projectile.Launcher.MapHeld, gas, (int)amount);
         }
     }
 }
